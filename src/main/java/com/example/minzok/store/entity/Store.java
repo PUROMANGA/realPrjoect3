bf9a3fa0bef58ec702894d268721dfd99e8d66f0
JIN <wlsqkrtk11@naver.com>
2025-04-22 20:16:15 +0900

    # <타입> : <제목> 형식으로 작성. 제목은 최대 50글자가 넘지 않도록 하고 # 제목을 아랫줄에 작성. 마침표X, 특수기호X, 영문 첫 글자 대문자, 현재시제
    #####################
    # 본문(추가 설명)을 아랫줄에 작성 (관련된 이슈 번호 등 함께)
    
    # feat
    Store entity 작성
    
    #####################
    # feat : 새로운 기능 추가
    # fix : 버그 수정
    # docs : 문서 수정
    # style : 코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
    # refactor : 코드 리펙토링
    # test : 테스트 코드, 리펙토링 테스트 코드 추가
    # chore : 빌드 업무 수정, 패키지 매니저 수정
    #####################

---
+++ src/main/java/com/example/minzok/store/entity/Store.java
@@ -0,0 +1,37 @@
+package com.example.minzok.store.entity;
+
+import com.example.minzok.global.base_entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Store")
+
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String Store_name;
+
+    @Column(nullable = false)
+    private String Store_content;
+
+    @Column(nullable = false)
+    private LocalDateTime openTime;
+
+    @Column(nullable = false)
+    private LocalDateTime closeTime;
+
+}
