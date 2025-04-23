a244d4ac076ea260916e4c525368cefa26b3f39f
Jong Hyeon Lee <ljh0990@naver.com>
2025-04-23 11:39:12 +0900

    menu made repository and entity

--- src/main/java/Menu/Entity/Menu.java
+++ src/main/java/Menu/Entity/Menu.java
@@ -1,8 +1,52 @@
 package Menu.Entity;
 
-import jakarta.persistence.Entity;
+import com.example.minzok.global.base_entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.catalina.Store;
 
+import java.util.ArrayList;
+
+@Getter
 @Entity
+@NoArgsConstructor
+@Table(name = "menus")
 
-public class Menu {
+public class Menu extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String menu_name;
+
+    @Column(nullable = false)
+    private Long price;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    /**
+     * 리뷰 연관관계
+     */
+
+    @OneToMany(mappedBy = "toMenu", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Reviews> reviews = New ArrayList<>();
+
+    public Menu(Name name ,Menu_name menu_name, Rating rating, Contents contents) {
+            this.name = name;
+            this.menu_name = menu_name;
+            this.rating = rating;
+            this.contents = contents;
+    }
+
+    public void update(menuRequestDto dto) {
+        this.contents = dto.getContents();
+    }
 }
--- src/main/java/Menu/Repository/MenuRepository.java
+++ src/main/java/Menu/Repository/MenuRepository.java
@@ -1,4 +1,20 @@
 package Menu.Repository;
 
-public class MenuRepository {
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+
+    slice<Menu> findByStoreId(Long storeId, Pageable pageable);
+
+    /**
+     *Store_Id로 메뉴를 찾아줄수있는 메소드이다.
+     * @param postId
+     * @param pageable
+     * @return
+     */
 }
