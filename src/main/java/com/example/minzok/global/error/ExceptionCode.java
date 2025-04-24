af178cf0def9ecacae4bf933f7ca1e1cf871336b
RKS-t <rudtjs5719@gmail.com>
2025-04-24 12:24:58 +0900

    feat : 토큰 필터 수정, 주소 생성 추가

--- src/main/java/com/example/minzok/global/error/ExceptionCode.java
+++ src/main/java/com/example/minzok/global/error/ExceptionCode.java
@@ -19,7 +19,8 @@ public enum ExceptionCode implements ErrorCode{
     TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
     CANT_FIND_TOKEN(HttpStatus.UNAUTHORIZED, "토큰을 찾지 못했습니다."),
     TOKEN_INVALID(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
-    TOKEN_BLACKLISTED(HttpStatus.UNAUTHORIZED, "로그아웃된 토큰입니다.");
+    TOKEN_BLACKLISTED(HttpStatus.UNAUTHORIZED, "로그아웃된 토큰입니다."),
+    ADDRESS_OVER(HttpStatus.BAD_REQUEST, "주소는 5개를 초과하여 저장할 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
