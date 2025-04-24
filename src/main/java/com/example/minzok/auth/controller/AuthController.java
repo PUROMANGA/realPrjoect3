cb45215b4b3f3fb7ac22f38fc52ab378da267685
RKS-t <rudtjs5719@gmail.com>
2025-04-24 11:02:09 +0900

    chore : 주석 추가

--- src/main/java/com/example/minzok/auth/controller/AuthController.java
+++ src/main/java/com/example/minzok/auth/controller/AuthController.java
@@ -21,16 +21,31 @@ public class AuthController {
 
     private final AuthService authService;
 
+    /**
+     * 입력한 정보로 회원가입 한다.
+     * @param dto
+     * @return
+     */
     @PostMapping("/signup")
     public ResponseEntity<TokenResponseDto> signup(@Valid @RequestBody SignUpRequestDto dto){
         return new ResponseEntity<>(authService.signup(dto), HttpStatus.CREATED);
     }
 
+    /**
+     * 입력한 정보로 로그인 한다.
+     * @param dto
+     * @return
+     */
     @PostMapping("/login")
     public ResponseEntity<TokenResponseDto> login(@Valid @RequestBody LoginRequestDto dto){
         return new ResponseEntity<>(authService.login(dto), HttpStatus.OK);
     }
 
+    /**
+     * 로그아웃하고 시큐리티 정보를 초기화 한다.
+     * @param token
+     * @return
+     */
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(@RequestHeader("Authorization") String token) {
         authService.logout(token);
