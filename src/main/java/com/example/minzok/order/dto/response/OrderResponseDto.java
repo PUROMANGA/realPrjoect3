0861d5eef4ec4ca133646ffcd9f185e13bb9d401
Kimyoonbeom <kimybeom@naver.com>
2025-04-24 11:58:04 +0900

    # feature: order의 service, controller 필수

--- src/main/java/com/example/minzok/order/dto/response/OrderResponseDto.java
+++ src/main/java/com/example/minzok/order/dto/response/OrderResponseDto.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 // 주문 생성 응답
 @Getter @Builder
@@ -13,4 +14,13 @@ public class OrderResponseDto {
     private int totalPrice;
     private String status;
     private LocalDateTime orderTime;
+    private List<OrderMResponseDto> menus;
+
+    @Getter
+    @Builder
+    public static class OrderMResponseDto{
+        private String menuName;
+        private int quantity;
+        private long price;
+    }
 }
