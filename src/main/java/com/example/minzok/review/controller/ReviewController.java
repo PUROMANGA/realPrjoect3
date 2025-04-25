f31c4edca0b6335191e74a4ad44c1584757d18bd
idkimjunyng <kimjunyng@icloud.com>
2025-04-25 14:46:35 +0900

    # fix: import MyUserDetail 버그 수정

--- src/main/java/com/example/minzok/review/controller/ReviewController.java
+++ src/main/java/com/example/minzok/review/controller/ReviewController.java
@@ -1,9 +1,8 @@
 package com.example.minzok.review.controller;
 
+import com.example.minzok.auth.entity.MyUserDetail;
 import com.example.minzok.global.error.CustomRuntimeException;
 import com.example.minzok.global.error.ExceptionCode;
-import com.example.minzok.global.jwt.MyUserDetail;
-import com.example.minzok.auth.entity.MyUserDetail;
 import com.example.minzok.member.entity.Member;
 import com.example.minzok.member.repository.MemberRepository;
 import com.example.minzok.order.entity.Order;
